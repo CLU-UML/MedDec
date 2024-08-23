7e9388282445ffb3cec4ced2741e04003eb0b181
Mohamed Elgaar <mohdelgaar@gmail.com>
2024-08-23 06:24:21 +0000

    cleanup

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-# MedDec: A Dataset for Extracting Medical Decisions from Clinical Narratives
+# MedDec: A Dataset for Extracting Medical Decisions from Discharge Summaries
 
-This is the code and dataset described in the [MedDec paper](/) (Findings of ACL: ACL 2024).
+![MedDec](assets/figure.png)
+
+This is the code and dataset described in **[MedDec (Elgaar et al., Findings of ACL: ACL 2024)](https://aclanthology.org/2024.findings-acl.975/)**.
+
+MedDec is the first dataset specifically developed for extracting and classifying medical decisions from clinical notes. It includes 451 annotated discharge summaries from the MIMIC-III dataset, offering a valuable resource for researchers focused on BioNLP and clinical decision-making.
 
 # Dataset
 
-The dataset is made available through this link: [https://physionet.org/TBD](https://physionet.org/). 
+The dataset is made available through this link: **[https://physionet.org/TBD](https://physionet.org/)**.  
+— <span style="color:red">Currently under review on PhysioNet. Please check back on September 15, 2024</span> —
 
 The user must sign a data usage agreement before accessing the dataset.
 
@@ -12,12 +17,63 @@ The user must sign a data usage agreement before accessing the dataset.
 
 The phenotype annotations used in the paper are available here: [https://physionet.org/content/phenotype-annotations-mimic/1.20.03/](https://physionet.org/content/phenotype-annotations-mimic/1.20.03/).
 
+# Installation
+
+```
+pip install -r requirements.txt
+```
+
 # Running the Baselines
 
+To train the baselines, run the following command:
 ```
 python main.py --label_encoding multiclass --model_name google/electra-base-discriminator --total_steps 5000 --lr 4e-5
 ```
 
+To evaluate the baselines, run the following command:
+```
+python main.py --eval_only --ckpt ./checkpoints/[datetime]-[model_name]
+```
+
+## Arguments
+
+- `data_dir`: The directory where the dataset is stored. The default is `./data/`.
+- `pheno_path`: The path to the phenotype annotations. The default is `./ACTdb102003.csv`.
+- `task`: `token` is the token classification task (decision extraction), and `seq` is the sequence classification task (phenotype prediction). The default is `token`.
+- `eval_only`: Whether to evaluate the model only. `--ckpt` should be provided. The default is `False`.
+- `label_encoding`: `multiclass`, `bo` (beginning inside outside), or `boe` (beginning outside end). The default is `multiclass`.
+- `truncate_train`: Truncate the training sequences to a maximum length. Otherwise, the sequences are randomly chunked at training time. The default is `False`.
+- `truncate_eval`: Truncate the evaluation sequences to a maximum length. The default is `False`.
+- `use_crf`: Whether to use a CRF layer. The default is `False`.
+- `model_name`: The name of the model from Hugging Face Transformers
+- `total_steps`: The number of training steps
+- `lr`: The learning rate
+- `batch_size`: The batch size
+- `seed`: The random seed
+
+
+
 # Citation
 
-TBA
+If you use this dataset or code, please cite the following paper:
+
+```
+@inproceedings{elgaar-etal-2024-meddec,
+    title = "{M}ed{D}ec: A Dataset for Extracting Medical Decisions from Discharge Summaries",
+    author = "Elgaar, Mohamed  and
+      Cheng, Jiali  and
+      Vakil, Nidhi  and
+      Amiri, Hadi  and
+      Celi, Leo Anthony",
+    editor = "Ku, Lun-Wei  and
+      Martins, Andre  and
+      Srikumar, Vivek",
+    booktitle = "Findings of the Association for Computational Linguistics ACL 2024",
+    month = aug,
+    year = "2024",
+    address = "Bangkok, Thailand and virtual meeting",
+    publisher = "Association for Computational Linguistics",
+    url = "https://aclanthology.org/2024.findings-acl.975",
+    pages = "16442--16455",
+}
+```
--- model.py
+++ model.py
@@ -1,10 +1,8 @@
-import copy
 import torch
 from torch import nn
 from transformers import AutoModel
 from torch.optim import AdamW
 from transformers import get_linear_schedule_with_warmup
-# from torchcrf import CRF
 
 class MyModel(nn.Module):
     def __init__(self, args, backbone):
@@ -62,107 +60,6 @@ class MyModel(nn.Module):
             h = torch.cat(outs, 1)
             return self.classifier(h)
 
-class CNN(nn.Module):
-    def __init__(self, args):
-        super().__init__()
-        self.emb = nn.Embedding(args.vocab_size, args.emb_size)
-        self.model = nn.Sequential(
-                nn.Conv1d(args.emb_size, args.hidden_size, args.kernels[0],
-                    padding='same' if args.task == 'token' else 'valid'),
-                nn.ReLU(),
-                nn.MaxPool1d(1),
-                nn.Conv1d(args.hidden_size, args.hidden_size, args.kernels[1],
-                    padding='same' if args.task == 'token' else 'valid'),
-                nn.ReLU(),
-                nn.MaxPool1d(1),
-                nn.Conv1d(args.hidden_size, args.hidden_size, args.kernels[2],
-                    padding='same' if args.task == 'token' else 'valid'),
-                nn.ReLU(),
-                nn.MaxPool1d(1),
-                )
-        if args.task == 'seq':
-            out_shape = 512 - args.kernels[0] - args.kernels[1] - args.kernels[2] + 3
-        elif args.task == 'token':
-            out_shape = 1
-        self.classifier = nn.Linear(args.hidden_size*out_shape, args.num_labels)
-        self.dropout = nn.Dropout()
-        self.args = args
-        self.device = None
-
-    def forward(self, x, _):
-        x = x.to(self.device)
-        bs = x.shape[0]
-        x = self.emb(x)
-        x = x.transpose(1,2)
-        x = self.model(x)
-        x = self.dropout(x)
-        if self.args.task == 'token':
-            x = x.transpose(1,2)
-            h = self.classifier(x)
-            return x, h
-        elif self.args.task == 'seq':
-            x = x.reshape(bs, -1)
-            x = self.classifier(x)
-            return x
-
-    def generate(self, x, _):
-        outs = []
-        for i, offset in enumerate(range(0, x.shape[1], self.args.max_len)):
-            segment = x[:, offset:offset + self.args.max_len]
-            n = segment.shape[1]
-            if n != self.args.max_len:
-                segment = torch.nn.functional.pad(segment, (0, self.args.max_len -  n))
-            if self.args.task == 'seq':
-                logits = self(segment, None)
-                outs.append(logits)
-            elif self.args.task == 'token':
-                h = self(segment, None)[0]
-                h = h[:,:n]
-                outs.append(h)
-        if self.args.task == 'seq':
-            return torch.max(torch.stack(outs, 1), 1).values
-        elif self.args.task == 'token':
-            h = torch.cat(outs, 1)
-            return self.classifier(h)
-
-class LSTM(nn.Module):
-    def __init__(self, args):
-        super().__init__()
-        self.emb = nn.Embedding(args.vocab_size, args.emb_size)
-        self.model = nn.LSTM(args.emb_size, args.hidden_size, num_layers=args.num_layers,
-                batch_first=True, bidirectional=True)
-        dim = 2*args.num_layers*args.hidden_size if args.task == 'seq' else 2*args.hidden_size
-        self.classifier = nn.Linear(dim, args.num_labels)
-        self.dropout = nn.Dropout()
-        self.args = args
-        self.device = None
-
-    def forward(self, x, _):
-        x = x.to(self.device)
-        x = self.emb(x)
-        o, (x, _) = self.model(x)
-        o_out = self.classifier(o) if self.args.task == 'token' else None
-        if self.args.task == 'seq':
-            x = torch.cat([h for h in x], 1)
-            x = self.dropout(x)
-            x = self.classifier(x)
-        return (x, o), o_out
-
-    def generate(self, x, _):
-        outs = []
-        for i, offset in enumerate(range(0, x.shape[1], self.args.max_len)):
-            segment = x[:, offset:offset + self.args.max_len]
-            if self.args.task == 'seq':
-                logits = self(segment, None)[0][0]
-                outs.append(logits)
-            elif self.args.task == 'token':
-                h = self(segment, None)[0][1]
-                outs.append(h)
-        if self.args.task == 'seq':
-            return torch.max(torch.stack(outs, 1), 1).values
-        elif self.args.task == 'token':
-            h = torch.cat(outs, 1)
-            return self.classifier(h)
 
 def load_model(args, device):
     if args.model == 'lstm':
@@ -177,6 +74,7 @@ def load_model(args, device):
         model.load_state_dict(torch.load(args.ckpt, map_location=device), strict=True)
     if args.label_encoding == 'multiclass':
         if args.use_crf:
+            from torchcrf import CRF
             crit = CRF(args.num_labels, batch_first = True).to(device)
         else:
             crit = nn.CrossEntropyLoss(reduction='none')
